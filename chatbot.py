4cad5a16b5368db24a706c662a35924d941037fc
Codebybhavani <bhavaniksla@gmail.com>
2025-03-11 18:22:04 +0530

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import google.generativeai as genai
-API_KEY = "AIzaSyDOqZVVnc-IPirAsofSxidr46jAsOoYqZk"
+API_KEY = "AIzaSyCExg1Y8pxdiwbGJ1EzKzN0CTb8sNhhQDI"
 
 genai.configure(api_key=API_KEY)
 model = genai.GenerativeModel('gemini-1.5-flash')
